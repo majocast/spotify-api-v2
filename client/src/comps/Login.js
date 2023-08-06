dc3396648d7cd498785f26e90437701b0ceba94c
Marc Joseph Guerrero Castro <marcjgcastro@gmail.com>
2023-08-05 18:09:29 -0700

    missing char redirect_uri string, testing deploy

--- client/src/comps/Login.js
+++ client/src/comps/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 
-const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=ed78470013204123a5c4a1501c41364e&response_type=code&redirect_uri=http://semiwrapped.onrender.com&scope=streaming%20user-read-email%20user-read-private%20user-top-read'
+const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=ed78470013204123a5c4a1501c41364e&response_type=code&redirect_uri=https://semiwrapped.onrender.com&scope=streaming%20user-read-email%20user-read-private%20user-top-read'
 
 const Login = () => {
   return (
